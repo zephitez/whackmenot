b4b7b228398a7fb93657b4a755b8c0a8d00da67d
Zhezhang Fu <zephitez@gmail.com>
2016-07-14 02:09:21 +0800

    finished with winning logic, only left cleaning up and no clicking together

--- script/whackme.js
+++ script/whackme.js
@@ -1,26 +1,34 @@
 $(document).ready(function() {
 
   function Game() {
-    this.rightAnsMsg = ['Awesome!', 'Good Job!', 'Nice!', 'Sweet'];
-    this.wrongAnsMsg = ['Wrong!', 'Oh Oh!', 'Uh.. Noooo..'];
+    this.rightAnsMsg = ['Awesome!', 'Good Job!', 'Nice!', 'Sweet!'];
+    this.wrongAnsMsg = ['Wrong!', 'Oh Oh!', 'Uh.. Noooo..', 'No Good!', 'That\'s a POISON PIG!'];
     this.playerWinMsg = 'Wins!';
     this.playerDraws = 'Its a Draw!';
     this.player1 = 'Player 1';
     this.player2 = 'Player 2';
+    this.timerDisplay = $('#countdown-timer');
+    this.randomImg = 0;
+    this.randomMsgNo = 0;
+    this.randomPopInterval = 0;
     this.player1Score = 0;
     this.player2Score = 0;
     this.timer = 30;
     this.countdownTimerId = 0;
     this.popUpTimerId = 0;
     this.popDownTimerId = 0;
+    this.image = ['images/angry-bird.png', 'images/poison-pig.png'];
+    this.happyfeetAudio = new Audio('audio/happy-feet.mp3');
+    this.winAudio = new Audio('audio/win.mp3');
+    this.loseAudio = new Audio('audio/lose.mp3');
     this.$clickStart = $('#start-btn');
     this.$clickRestart = $('#restart-btn');
-    this.$remarks = $('#remarks');
+    this.$response = $('#response');
+    this.$instruction = $('.instruction');
     this.$popImageBox = $('#box1');
-    this.timerDisplay = $('#countdown-timer');
-    this.image = ['images/angry-bird.png', 'images/poison-pig.png'];
-    this.randomImg = 0;
-    this.randomPopInterval = 0;
+    this.$player1 = $('#player1');
+    this.$player2 = $('#player2');
+
   }
 
   Game.prototype = {
@@ -31,34 +39,32 @@ $(document).ready(function() {
       this.$clickStart.click(this.initGame.bind(this));
 
       // adding keypress event listener to all answers -> when key is pressed, it updates score
-      // this.$keyPress.keypress($.proxy(this.whenKeyDown, this));
-      $(document).keypress(this.whenKeyDown.bind(this));
+      $(document).keypress(this.updateScore.bind(this));
 
-      // adding click event liand it calls the restart function
+      // adding click event listener --> it calls the restart function
       this.$clickRestart.click(this.restart.bind(this));
     },
 
     initGame: function() {
-      //hide Start button
+      //hide Start button and instructions, clear response box
       this.$clickStart.hide();
+      this.$instruction.hide();
+      this.setMsg(this.$response, 'Let\'s Start!');
       //play music
-      this.playGameMusic();
+      this.happyfeetAudio.load();
+      this.happyfeetAudio.play();
+      //set Timers
       this.setTimerInterval();
       this.setPopUpInterval();
-      this.isGameOver();
     },
 
-    playGameMusic: function() {
-      var happyfeet = new Audio('audio/happy-feet.mp3');
-      happyfeet.play();
-    },
-
-    //insert images
+    //insert image
     popImageUp: function() {
       this.randomImg = Math.floor(Math.random() * 2);
       this.$popImageBox.attr('src', this.image[this.randomImg]);
     },
 
+    //remove image
     popImageDown: function() {
       this.$popImageBox.attr('src', ' ');
     },
@@ -70,60 +76,100 @@ $(document).ready(function() {
 
     //countdown by 1 second
     countDownTimer: function() {
-      console.log(this.timer);
-      if (this.timer !== -1) {
-      this.timer--;
-      this.setMsg(this.timerDisplay, 'Countdown \: ' + this.timer);
-    }
+      if (this.timer !== 0) {
+        this.timer--;
+        this.setMsg(this.timerDisplay, 'Countdown \: ' + this.timer);
+      } else {
+        this.isGameOver();
+      }
     },
 
     //set image popping up Interval
     setPopUpInterval: function() {
       this.randomPopInterval = Math.floor(Math.random() * 500) + 500;
-
       this.popUpTimerId = window.setInterval(this.popImageUp.bind(this), this.randomPopInterval);
       this.setPopDownInterval();
     },
 
+    //set image popping down Interval
     setPopDownInterval: function() {
       this.randomPopInterval = Math.floor(Math.random() * 1500) + 1000;
-
       this.popDownTimerId = window.setInterval(this.popImageDown.bind(this), this.randomPopInterval);
     },
 
-    whenKeyDown: function(event) {
+    //update score and display wrong or right message
+    updateScore: function(event) {
 
-      // boingMusic.play();
+
       if (this.isGameOver()) {
-        // this.whoWon();
+        this.whoWon();
       } else {
-        // switch (event.which)
-        console.log(event);
-        console.log(event.which);
+        //create random right and wrong answer message
+        this.randomMsgNo = Math.floor(Math.random() * 4);
 
-
-
-
+        //update score for player 1 and 2
+        switch (event.which) {
+          case 97:
+            if (this.$popImageBox.attr('src') == this.image[0]) {
+              this.player1Score += 3;
+              this.setMsg(this.$player1, this.player1Score);
+              this.setMsg(this.$response, this.rightAnsMsg[this.randomMsgNo]);
+              this.winLoseAudio('win');
+            } else if (this.$popImageBox.attr('src') == this.image[1]) {
+              this.player1Score -= 2;
+              this.setMsg(this.$player1, this.player1Score);
+              this.setMsg(this.$response, this.wrongAnsMsg[4]);
+              this.winLoseAudio();
+            } else {
+              this.player1Score--;
+              this.setMsg(this.$player1, this.player1Score);
+              this.setMsg(this.$response, this.wrongAnsMsg[this.randomMsgNo]);
+            }
+            break;
+          case 108:
+            if (this.$popImageBox.attr('src') == this.image[0]) {
+              this.player2Score += 3;
+              this.setMsg(this.$player2, this.player2Score);
+              this.setMsg(this.$response, this.rightAnsMsg[this.randomMsgNo]);
+              this.winLoseAudio('win');
+            } else if (this.$popImageBox.attr('src') == this.image[1]) {
+              this.player2Score -= 2;
+              this.setMsg(this.$player2, this.player2Score);
+              this.setMsg(this.$response, this.wrongAnsMsg[4]);
+              this.winLoseAudio();
+            } else {
+              this.player2Score--;
+              this.setMsg(this.$player2, this.player2Score);
+              this.setMsg(this.$response, this.wrongAnsMsg[this.randomMsgNo]);
+            }
+            break;
+        }
       }
     },
-    //set Message to timer and message Box
-    setMsg: function(destination, message) {
-      destination.text(message);
-    },
+
+winLoseAudio: function(x) {
+  if (x == 'win') {
+    console.log(x);
+    this.loseAudio.pause();
+    this.winAudio.load();
+    this.winAudio.play();
+  } else {
+    this.winAudio.pause();
+    this.loseAudio.load();
+    this.loseAudio.play();
+  }
+},
 
     // check winner
     whoWon: function() {
-      // if (this.player1Score > this.player2Score) {
-      //   this.updateRemarks(this.player1 + ' ' + this.playerWinMsg);
-      //
-      //
-      // } else if (this.player1Score < this.player2Score) {
-      //   this.updateRemarks(this.player2 + ' ' + this.playerWinMsg);
-      // } else {
-      //   this.setMsg(this.$remarks, "It\'s a Draw Lah!");
-      //
-      // }
+      if (this.player1Score > this.player2Score) {
+        this.setMsg(this.$response, this.player1 + ' ' + this.playerWinMsg);
 
+      } else if (this.player1Score < this.player2Score) {
+        this.setMsg(this.$response, this.player2 + ' ' + this.playerWinMsg);
+      } else {
+        this.setMsg(this.$response, this.playerDraws);
+      }
     },
 
     //check if timer is zero, then gameover --> and make images stop popping
@@ -140,19 +186,33 @@ $(document).ready(function() {
       window.clearInterval(timerId);
     },
 
+    //set Message to timer and message Box
+    setMsg: function(destination, message) {
+      destination.text(message);
+    },
+
     //restarts the game
     restart: function() {
       this.clearIntervalOfId(this.countdownTimerId);
       this.timer = 30;
       this.$clickStart.show();
+      this.$instruction.show();
       this.setMsg(this.timerDisplay, 'Countdown \: ' + this.timer);
-
+      this.happyfeetAudio.pause();
+      this.clearIntervalOfId(this.popDownTimerId);
+      this.clearIntervalOfId(this.popUpTimerId);
+      this.popImageDown();
+      this.winAudio.load();
+      this.loseAudio.load();
+      this.player1Score = 0;
+      this.player2Score = 0;
+      this.setMsg(this.$player1, this.player1Score);
+      this.setMsg(this.$player2, this.player2Score);
     }
   };
 
 
   var angryBird = new Game();
-
   angryBird.setUpGame();
 
   // document ready ends here
